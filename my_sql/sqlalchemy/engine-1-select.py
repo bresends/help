eee91135d692b0e698ffc42bb11b1c6dffbcd13c
Bruno Resende dos Santos <64028125+bresends@users.noreply.github.com>
2020-06-02 20:13:58 -0300

    Update engine-1-select.py

--- my_sql/sqlalchemy/engine-1-select.py
+++ my_sql/sqlalchemy/engine-1-select.py
@@ -1,25 +1,24 @@
 from sqlalchemy import create_engine
 
-#engine = create_engine("sqlite:///some.db") # create sqlite db. use "////" to specify absolute path
-engine = create_engine("sqlite:///:memory:", echo=True)
+engine = ("mysql+mysqlconnector://root:74Q50c$0rIZ&GDhp@localhost:3306/stocks", echo=False)
 
-# create table
+# Create table
 engine.execute("""CREATE TABLE emp (
 id INTEGER PRIMARY KEY,
 name VARCHAR(50)
 )""")
 
-# insert data (connectionless execution (via engine))
+# Insert data (connectionless execution (via engine))
 engine.execute("INSERT INTO emp (name) VALUES (:name)", name="Slavo")
 engine.execute("INSERT INTO emp (name) VALUES (:name)", name="Jano")
 engine.execute("INSERT INTO emp (name) VALUES (:name)", name="Vlado")
 
-# query data
+# Query data
 result = engine.execute("select id, name from emp")
 rows = result.fetchall()
 result.close(); # should be done automatically
 
-# display data
+# Display data
 for row in rows: # we could also iterate directly over result without need to call fetchall()
     print("DATA: " + repr(row))
 
